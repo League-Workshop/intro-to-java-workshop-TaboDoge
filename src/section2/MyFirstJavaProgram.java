367fb6c2c8d815b5d29785df629c56647e547536
An idiot <ur@mom.com>
2019-03-05 18:28:54 -0800

    day 2

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,6 +8,24 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+	Robot pewdiepie = new Robot();
+	pewdiepie.penDown();
+	pewdiepie.setSpeed(100000);
+	pewdiepie.setPenWidth(10000000);
+	for(int i=0; 1<2;i++)
+	{
+		
+		
+		pewdiepie.move(100);
+		pewdiepie.turn(90);
+		pewdiepie.move(100);
+		pewdiepie.turn(90);
+		pewdiepie.move(100);
+		pewdiepie.turn(90);
+		pewdiepie.move(100);
+		
+		
+	}
 	
 		
 		
