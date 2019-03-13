e7f5429c93fa024ca7aa02286abb5952f95a9105
varnit05 <League@AdministorsiMac.attlocal.net>
2019-03-12 18:24:36 -0700

    Day 3

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -6,23 +6,17 @@ import org.jointheleague.graphical.robot.Robot;
 public class MyFirstJavaProgram {
 	
 	public static void main(String[] args) {
+
 		
 		// START HERE
 	Robot pewdiepie = new Robot();
 	pewdiepie.penDown();
-	pewdiepie.setSpeed(100000);
+	pewdiepie.setSpeed(100);
 	pewdiepie.setPenWidth(10000000);
-	for(int i=0; 1<2;i++)
+	for(int i=0; 1<10;i++)
 	{
 		
-		
-		pewdiepie.move(100);
-		pewdiepie.turn(90);
-		pewdiepie.move(100);
-		pewdiepie.turn(90);
-		pewdiepie.move(100);
-		pewdiepie.turn(90);
-		pewdiepie.move(100);
+
 		
 		
 	}
